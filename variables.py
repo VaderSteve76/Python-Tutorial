348b61c53ff6e70ab1058db4428d57afea653b62
VaderSteve76 <alverson.s@yahoo.com>
2019-05-26 21:26:05 -0400

    Variable Comments

---
+++ variables.py
@@ -0,0 +1,15 @@
+# a variable is a container for a value, which can be of various types
+
+'''
+this is a
+multi line comment
+or docstring (used to define a functions purpose)
+an be single or double quotes
+'''
+
+"""
+variable rules:
+- Variable names are case sensitive (name and NAME are different variables)
+- must start with a letter or an underscore
+- can have numbers but not start with one
+"""
