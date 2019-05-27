f0604f96a4a49b43b027401bc3d0eba7696e65fd
VaderSteve76 <alverson.s@yahoo.com>
2019-05-26 22:24:32 -0400

    Variables

--- variables.py
+++ variables.py
@@ -13,3 +13,26 @@ variable rules:
 - must start with a letter or an underscore
 - can have numbers but not start with one
 """
+
+'''
+x = 1            # int
+y = 2.5          # float
+name = 'Steve'   # str
+is_cool = True   # boolean
+'''
+
+# multiple assignment
+x, y, name, is_cool = (1, 2.5, 'Steve', True)
+
+# basic math
+a = x + y
+
+# casting
+x = str(x)
+y = int(y)
+z = float(y)
+
+print(type(x))
+print(type(y), y)
+print(type(z), z)
+print(x, y, name, is_cool, a)
